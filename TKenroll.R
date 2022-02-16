d20e6bcb8df6ba3124e61ab544e16044efd1f8f8
David Dobrowski <dobrowski@gmail.com>
2022-02-16 09:10:33 -0800

    Revised version, plus data sources and webapp

--- TKenroll.R
+++ TKenroll.R
@@ -6,8 +6,7 @@ library(here)
 library(googlesheets4)
 library(lubridate)
 library(readxl)
-
-
+library(ggthemes)
 
 con <- MCOE::mcoe_sql_con()
 
@@ -22,7 +21,12 @@ con <- MCOE::mcoe_sql_con()
 #     select(`School Year`,
 #            Kindergarten) %>%
 #     mutate(perc = Kindergarten/)
-    
+
+### Population Estimates ---- 
+
+# P2B_County_Age for projection of 4 year olds compared to 2020.  
+# From https://www.dof.ca.gov/forecasting/demographics/projections/
+
 pop.4.proj <- read_excel("P2B_County_Age.xlsx",
                        sheet = "County Population by Age",
                        range = "A3:BA3365") # Doens't get all counties
@@ -33,6 +37,8 @@ mry.4.proj <- pop.4.proj %>%
     pivot_longer(cols = `2010`:`2060`) %>%
     mutate(perc.of.2019 = value/6366)
 
+
+### Births ----
 # https://data.chhs.ca.gov/dataset/cdph_live-birth-by-zip-code
 
 births <- read_csv("2021-10-18_births_final_zip_year_sup.csv")
@@ -42,7 +48,7 @@ c(95076L, 93906L, 93905L, 93955L, 93940L, 93901L, 93960L, 93933L, 93907L, 93927L
 
 births.mry <- births %>%
     filter(ZIP_Code %in% mry.zips,
-           Year >= 2012
+           Year >= 2008
            ) 
 
 birth.by.year <- births.mry %>%
@@ -55,74 +61,12 @@ ggplot(births.mry, aes(x = Year, y = Count, group = ZIP_Code))+
     geom_line()
 
 
-sas <- haven::read_sas("EDGE_ZCTALOCALE_2021_LOCALE.sas7bdat")
-
-# https://nces.ed.gov/programs/edge/Geographic/RelationshipFiles
-school.zip <- read_excel("grf21_lea_zcta5ce20.xlsx")
-
-
-school.zip.mry <- school.zip %>%
-    filter(ZCTA5CE20 %in% mry.zips)
-
-write.csv(school.zip.mry, "School Zip crosswalk.csv")
-
-
-schools <- read_csv("School Zip crosswalk.csv")
-
-
-
-zip.births.mry <- schools %>%
-    left_join(births.mry, by = c("ZCTA5CE20" = "ZIP_Code")) %>%
-    group_by(NAME_LEA21, Year) %>%
-    transmute(babies = sum(Count, na.rm = TRUE)) %>%
-    unique() %>%
-    mutate(short.name = str_sub(NAME_LEA21 ,1,6),
-           year.of.birth = Year)
-
-enrollment.sum.old <- enrollment.sum %>%
-    mutate(year.of.birth = as.numeric(YEAR) + 1995,
-           year.of.five = as.numeric(YEAR) + 2000,
-           short.name = str_sub(DISTRICT ,1,6)) %>%
-    filter(!str_detect(DISTRICT,"High"),
-           !str_detect(DISTRICT,"Education"))
-
-zip.births.mry2  <- zip.births.mry %>%
-    left_join(enrollment.sum.old)
-
-lm.model <- zip.births.mry2 %>%
-    group_by(NAME_LEA21) %>%
-    nest() %>%
-    mutate(lm_obj =  map(data, ~lm(k.tot~babies, data = .x))) %>%
-    mutate(prediction = map2(lm_obj, data, function(.lm, .data) predict(.lm, .data) )) %>%
-    unnest(cols = c(data, prediction)) 
-           
-
-
-#  TK files https://www.cde.ca.gov/ds/ad/filestkdata.asp
-
-TK <- tbl(con, "TK") %>%
-    filter(Year == max(Year),
-           Reporting_Level == "District",
-           Race =="All",
-           Gender == "All",
-           Subgroup == "All") %>%
-    collect() 
-
-# P2B_County_Age for projection of 4 year olds compared to 2020.  
-# From https://www.dof.ca.gov/forecasting/demographics/projections/
-
-TK2 <- TK %>%
-    filter( str_starts(CDS_Code,"27")
-    ) %>%
-    select(Name, Total_Kindergarten_Enrollment_Census_Day, Kindergarten_TK_Program_Participation_Census_Day) %>%
-    mutate(k_without_TK = Total_Kindergarten_Enrollment_Census_Day - Kindergarten_TK_Program_Participation_Census_Day)
-
-
+### School Enrollment -------
 #  From https://www.cde.ca.gov/ds/ad/filesenr.asp
 
 ENROLL <- tbl(con, "ENROLLMENT") %>%
     filter(#YEAR == "19", # To match the last TK data file year 19-20
-           COUNTY == "Monterey") %>%
+        COUNTY == "Monterey") %>%
     collect() 
 
 
@@ -131,35 +75,174 @@ enrollment.sum <- ENROLL %>%
     summarise(k.tot = sum(KDGN),
               gr1.tot = sum(GR_1))
 
-enrollment.sum.19 = enrollment.sum %>%
+enrollment.sum.19 <- enrollment.sum %>%
     filter(YEAR == "19")
 
+#  TK files https://www.cde.ca.gov/ds/ad/filestkdata.asp
 
-joint <- left_join(TK2, enrollment.sum.19, by = c("Name" = "DISTRICT")
-                   ) %>%
-    left_join(lm.model) %>%
-#    group_by(Name) %>%
-    mutate(est.tk20.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2020),"perc.of.2019"])*gr1.tot*as.numeric(mdy("12/2/2016") - mdy("9/2/2016"))/365, 0 ), 
-           est.tk23.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2023),"perc.of.2019"])*gr1.tot*as.numeric(mdy("2/2/2018") - mdy("9/2/2017"))/365, 0 ), 
-           est.tk24.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2024),"perc.of.2019"])*gr1.tot*as.numeric(mdy("4/2/2019") - mdy("9/2/2018"))/365, 0 ),
-           est.tk25.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2025),"perc.of.2019"])*gr1.tot*as.numeric(mdy("6/2/2019") - mdy("9/2/2018"))/365, 0 ),
+TK <- tbl(con, "TK") %>%
+    filter(#Year == max(Year),
+           Reporting_Level == "District",
+           Race =="All",
+           Gender == "All",
+           Subgroup == "All") %>%
+    collect() 
+
+TK2 <- TK %>%
+    filter( str_starts(CDS_Code,"27")
+    ) %>%
+    select(Year, Name, Total_Kindergarten_Enrollment_Census_Day, Kindergarten_TK_Program_Participation_Census_Day) %>%
+    mutate(k_without_TK = Total_Kindergarten_Enrollment_Census_Day - Kindergarten_TK_Program_Participation_Census_Day)
+
+
+
+###  School Districts to Zip Codes ----
+# https://nces.ed.gov/programs/edge/Geographic/RelationshipFiles
+# school.zip <- read_excel("grf21_lea_zcta5ce20.xlsx")
+# 
+# school.zip.mry <- school.zip %>%
+#     filter(ZCTA5CE20 %in% mry.zips)
+# 
+# write.csv(school.zip.mry, "School Zip crosswalk.csv")
+
+#  My reduced version with only primary 
+schools <- read_csv("School Zip crosswalk.csv")
+
+### Joining Schools to Births ----
+
+zip.births.mry <- schools %>%
+    left_join(births.mry, by = c("ZCTA5CE20" = "ZIP_Code")) %>%
+    group_by(NAME_LEA21, year.of.birth = Year) %>%
+    transmute(babies = sum(Count, na.rm = TRUE)) %>%
+    unique() %>%
+    mutate(short.name = str_sub(NAME_LEA21 ,1,6)  )
+
+joiner <- TK2 %>%
+    mutate(year.of.birth = as.numeric(str_sub(Year, 1,4)) -5, 
+           short.name = str_sub(Name ,1,6)
+    ) %>%
+    filter(!str_detect(Name,"Education")) %>%
+    full_join(zip.births.mry) %>%
+    select(-Name)
+
+# 
+# 
+# enrollment.sum.old <- enrollment.sum %>%
+#     mutate(year.of.birth = as.numeric(YEAR) + 1995,
+#            year.of.five = as.numeric(YEAR) + 2000,
+#            short.name = str_sub(DISTRICT ,1,6)) %>%
+#     filter(!str_detect(DISTRICT,"High"),
+#            !str_detect(DISTRICT,"Education"))
+# 
+# zip.births.mry2  <- zip.births.mry %>%
+#     left_join(enrollment.sum.old)
+
+# lm.model <- zip.births.mry2 %>%
+#     group_by(NAME_LEA21) %>%
+#     nest() %>%
+#     mutate(lm_obj =  map(data, ~lm(k.tot~babies, data = .x))) %>%
+#     mutate(prediction = map2(lm_obj, data, function(.lm, .data) predict(.lm, .data) )) %>%
+#     unnest(cols = c(data, prediction)) 
+           
+
+lm.model <- joiner %>%
+    group_by(NAME_LEA21) %>%
+    nest() %>%
+    mutate(lm_obj =  map(data, ~lm(k_without_TK~babies, data = .x))) %>%
+    mutate(prediction = map2(lm_obj, data, function(.lm, .data) predict(.lm, .data) )) %>%
+    mutate(graph =  map(data,
+                        ~ggplot(aes(y = k_without_TK, x =babies, label = year.of.birth), data = .x) +
+                            theme_hc() +
+                            geom_point() +
+                            geom_smooth(method="lm", se = FALSE) +
+                            geom_label() +
+                            # expand_limits(y = c(0,10),
+                            #               x = c(0,10)) +
+                            labs(title = NAME_LEA21[1],
+                                 x = "Number of babies born in associated zip code(s)",
+                                 y = "Kinder enrollment excluding TK 5 years later")
+                        )) %>%
+    mutate(graph.births =  map(data,
+                        ~ggplot(aes(y = babies, x = (year.of.birth)), data = .x) +
+                            theme_hc() +
+                            geom_line() +
+                            scale_x_continuous(breaks = 2008:2020) +
+#                   expand_limits(x = c(2008,2020)) +
+                   labs(title = NAME_LEA21[1],
+                        x = "Year",
+                        y = "Number of Births in Associated Zip Code(s)")
+    )) %>%
+    unnest(cols = c(data, prediction)) %>%
+    arrange(NAME_LEA21,Year)
+
+
+
+lm.predicts <- lm.model %>%
+    select(NAME_LEA21, year.of.birth, prediction2023 = prediction) %>%
+    filter(year.of.birth == 2018) %>%
+    select(-year.of.birth) %>%
+    mutate(Year = "2019-20")
+
+
+###
+# 
+# joint <- left_join(TK2, enrollment.sum.19, by = c("Name" = "DISTRICT")
+#                    ) %>%
+#     left_join(lm.model) %>%
+# #    group_by(Name) %>%
+#     mutate(est.tk20.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2020),"perc.of.2019"])*gr1.tot*as.numeric(mdy("12/2/2016") - mdy("9/2/2016"))/365, 0 ), 
+#            est.tk23.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2023),"perc.of.2019"])*gr1.tot*as.numeric(mdy("2/2/2018") - mdy("9/2/2017"))/365, 0 ), 
+#            est.tk24.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2024),"perc.of.2019"])*gr1.tot*as.numeric(mdy("4/2/2019") - mdy("9/2/2018"))/365, 0 ),
+#            est.tk25.gr1enr = round2(pull(mry.4.proj[(mry.4.proj$name == 2025),"perc.of.2019"])*gr1.tot*as.numeric(mdy("6/2/2019") - mdy("9/2/2018"))/365, 0 ),
+#            est.tk23.TKactual = round2(pull(mry.4.proj[(mry.4.proj$name == 2020),"perc.of.2019"])* Kindergarten_TK_Program_Participation_Census_Day* as.numeric(mdy("2/2/2018") - mdy("9/2/2017")) / as.numeric(mdy("12/2/2016") - mdy("9/2/2016")),0 ),
+#     )  %>%
+#     arrange(Name) %>%
+#     mutate(
+#         est.tk23.mean = round2( mean(c(est.tk23.gr1enr,est.tk23.TKactual)),0 ) ,
+#         est.tk23.teachers = ceiling( est.tk23.mean/12 )
+#         
+#     )# %>%
+# #    ungroup()
+# 
+# joint2 <- left_join(joint,lm.model)
+
+enrollment.sum.20 <- enrollment.sum %>%
+    filter(YEAR == 20,
+           !str_detect(DISTRICT,"Education"),
+           !str_detect(DISTRICT,"High")) %>%
+    transmute(year.in.first = "2020-21",
+           Year = "2019-20",
+           short.name = str_sub(DISTRICT ,1,6),
+           gr1.tot = gr1.tot
+    )
+#  Check if there is a duplication with Salinas High and MCOE first thing!!!!
+joint <- lm.model %>%
+    left_join(enrollment.sum.20) %>%
+    left_join(lm.predicts) %>%
+    filter(Year == "2019-20") %>%
+    mutate(est.tk20.county = round2(pull(mry.4.proj[(mry.4.proj$name == 2020),"perc.of.2019"])*gr1.tot*as.numeric(mdy("12/2/2016") - mdy("9/2/2016"))/365, 0 ), 
+           est.tk23.county = round2(pull(mry.4.proj[(mry.4.proj$name == 2023),"perc.of.2019"])*gr1.tot*as.numeric(mdy("2/2/2018") - mdy("9/2/2017"))/365, 0 ), 
+           est.tk24.county = round2(pull(mry.4.proj[(mry.4.proj$name == 2024),"perc.of.2019"])*gr1.tot*as.numeric(mdy("4/2/2019") - mdy("9/2/2018"))/365, 0 ),
+           est.tk25.county = round2(pull(mry.4.proj[(mry.4.proj$name == 2025),"perc.of.2019"])*gr1.tot*as.numeric(mdy("6/2/2019") - mdy("9/2/2018"))/365, 0 ),
            est.tk23.TKactual = round2(pull(mry.4.proj[(mry.4.proj$name == 2020),"perc.of.2019"])* Kindergarten_TK_Program_Participation_Census_Day* as.numeric(mdy("2/2/2018") - mdy("9/2/2017")) / as.numeric(mdy("12/2/2016") - mdy("9/2/2016")),0 ),
-    )  %>%
-    arrange(Name) %>%
+           est.tk23.zip = round2(prediction2023*as.numeric(mdy("2/2/2018") - mdy("9/2/2017"))/365, 0 ), 
+           )  %>%
     mutate(
-        est.tk23.mean = round2( mean(c(est.tk23.gr1enr,est.tk23.TKactual)),0 ) ,
+        est.tk23.mean = round2( mean(c(est.tk23.county,est.tk23.TKactual,est.tk23.zip)),0 ) ,
         est.tk23.teachers = ceiling( est.tk23.mean/12 )
         
-    )# %>%
-#    ungroup()
-
-joint2 <- left_join(joint,lm.model)
+    ) %>%
+    ungroup() %>%
+    arrange(NAME_LEA21)
 
 
 ss <- "https://docs.google.com/spreadsheets/d/1p5EPFn9zkZGClmYPFC09cin8QzBFS5ZVa9y0O5GO84c/edit#gid=1739770238"
 
 write_sheet(joint, ss = ss)
 write_rds(joint,here("TKestimates","joint.rds"))
+
+write_rds(schools,here("TKestimates","schools.rds"))
+
 
 #  Total population changes 
 #  Preschool enrollment
---
+++ TKestimates/app.R
@@ -0,0 +1,133 @@
+#
+
+library(shiny)
+library(tidyverse)
+
+joint <- read_rds("joint.rds")
+
+schools <- read_rds("schools.rds")
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Monterey County Estimates for future TK Enrollment LEAs"),
+    
+    # Sidebar to select LEA
+    sidebarLayout(
+        sidebarPanel(
+            selectInput("select", label = h3("Select your LEA"),
+                        choices = joint$NAME_LEA21,
+                        selected = 1),
+        ),
+        
+        # Main Bofy, in this case text output
+mainPanel(
+    p("This estimates the number of TK students that will be enrolled in your school for each of the following four years. It is based on prior TK enrollment, prior 1st grade enrollment and births in zip codes associated with your LEA. If you would like to see the specific calculations, please let me know."),
+               htmlOutput("text"),
+               br(),
+    htmlOutput("sentence2"),
+    htmlOutput("zips"),
+    p(""),
+    plotOutput("births"),
+    p(""),
+    plotOutput("ratio"),
+    p(""),
+    br(),
+    h4("Sources:"),
+    a("California Department of Education Downloadable Data File on Transitional Kindergarten Enrollment",href="     https://www.cde.ca.gov/ds/ad/filestkdata.asp"),
+    br(),
+    a("California Department of Education Downloadable Data File on Census Day Enrollment",href="     https://www.cde.ca.gov/ds/ad/filesenr.asp"),
+    br(),
+    a("National Center for Education Statistics Datafiles comparing LEAs to Zip Codes",href="     https://nces.ed.gov/programs/edge/Geographic/RelationshipFiles"),
+    br(),
+    a("California Health and Human Services Records of Births by Zip Code",href="     https://data.chhs.ca.gov/dataset/cdph_live-birth-by-zip-code"),
+    br(),
+    a("California Department of Finance County Population Projections",href="     https://www.dof.ca.gov/forecasting/demographics/projections/"),
+    br(),
+    img(src='logo.png', height="15%", width="15%", align = "right")
+    
+)
+        )
+    )
+
+# Define server logic 
+server <- function(input, output) {
+
+    jointInput <- reactive({
+        joint %>%
+            filter(NAME_LEA21 == input$select) 
+    })
+    
+    schoolsInput <- reactive({
+        schools %>%
+            filter(NAME_LEA21 == input$select) 
+    })
+    
+    
+    output$text <- renderText({
+        joint2 <- jointInput()
+        
+        paste0("<h4>For ",
+               input$select,
+               " there will be an estimated <b>",
+               joint2 %>%
+                   select(est.tk23.mean) ,
+               "</b> students in 2023.  They will require about ",
+               joint2 %>%
+                   select(est.tk23.teachers) ,
+               " teachers.</h4>")
+    })
+    
+    
+    
+    output$zips <- renderText({
+        schools2 <- schoolsInput()
+        
+        paste0("For ",
+               input$select,
+               " the following zip codes were considered to be associated: ",
+               schools2 %>%
+                   select(ZCTA5CE20),
+               ".")
+    })
+    
+    
+    
+    output$sentence2 <- renderText({
+        joint2 <- jointInput()
+        
+        paste0("The best estimate used is the average of three estimates.  One estimate is based on the actual number of 2019-20 TK students (",
+               joint2 %>%
+                   select(est.tk23.TKactual) ,
+               "), a second estimate is based on changes in county population projections (",
+               joint2 %>%
+                   select(est.tk23.county) ,
+               ") and a third estimate is based on the number of births in zip codes associated with the LEA (",
+                              joint2 %>%
+                   select(est.tk23.zip) %>%
+                   unlist(),
+               ").")
+    })
+    
+    
+    output$births <- renderPlot({
+        joint2 <- jointInput()
+        
+        joint2[[12]][[1]]
+        
+    })
+    
+    
+    output$ratio <- renderPlot({
+        joint2 <- jointInput()
+        
+        joint2[[11]][[1]]
+        
+    })
+    
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
